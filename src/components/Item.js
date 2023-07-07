82bd03901f39d91d9ec20012aacf74c88dcd6da1
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-07 16:39:43 -0700

    rf Item WIP

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,9 +3,10 @@ import React from 'react';
 function Item({ num, title, text }) {
 	return (
 		<div className='item'>
-			<p>{num}</p>
-			<h2>{title}</h2>
-			<p>{text}</p>
+			<p className='number'>{num < 9 ? `0${num}` : num}</p>
+			<h2 className='text'>{title}</h2>
+			<p className='icon'>-</p>
+			<div className='content-box'>{text}</div>
 		</div>
 	);
 }

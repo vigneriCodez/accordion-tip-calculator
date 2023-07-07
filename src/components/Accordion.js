d9e22cabef7a818ff71c1e970cffa4a6a3c39188
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-07 16:38:19 -0700

    rf to use implicit return, almost went with arrow also

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,21 +2,14 @@ import React from 'react';
 
 import Item from './Item';
 
-function Accordion({ faqs }) {
+function Accordion({ data }) {
 	return (
 		<div className='accordion'>
 			<h1>the Accordion:</h1>
 			<ol>
-				{faqs.map((faq, i) => {
-					return (
-						<Item
-							key={faq.title}
-							title={faq.title}
-							text={faq.text}
-							num={i + 1}
-						/>
-					);
-				})}
+				{data.map((el, i) => (
+					<Item key={el.title} title={el.title} text={el.text} num={i + 1} />
+				))}
 			</ol>
 		</div>
 	);

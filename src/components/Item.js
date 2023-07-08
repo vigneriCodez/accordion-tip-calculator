4ec0cdd8e1e9f45e7ea369137c26e5588263d4ab
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-07 17:50:36 -0700

    add isOpen piece of state and basic functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Item({ num, title, text }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		setIsOpen((bool) => !bool);
+	};
+
 	return (
-		<div className='item'>
+		<div onClick={handleToggle} className={`item ${isOpen ? 'open' : ''}`}>
 			<p className='number'>{num < 9 ? `0${num}` : num}</p>
-			<h2 className='text'>{title}</h2>
-			<p className='icon'>-</p>
-			<div className='content-box'>{text}</div>
+			<h2 className='title'>{title}</h2>
+			<p className='icon'>{isOpen ? '-' : '+'}</p>
+			{isOpen && <div className='content-box'>{text}</div>}
 		</div>
 	);
 }

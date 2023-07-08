e854355a3d3cc820d3fe78b2673c675b49ff76d0
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-08 16:13:58 -0700

    add Tip Calculator Control (main) component -mostly complete

--- src/components/TipCalculatorControl.js
+++ src/components/TipCalculatorControl.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import InputBill from './InputBill';
 import TipService from './TipService';
@@ -6,20 +6,30 @@ import CalcDisplay from './CalcDisplay';
 import ResetButton from './ResetButton';
 
 function TipCalculatorControl() {
+	const [billAmt, setBillAmt] = useState(0);
+	const [tip1Amt, setTip1Amt] = useState(5);
+	const [tip2Amt, setTip2Amt] = useState(5);
+
+	const handleResetPress = () => {
+		setBillAmt(0);
+		setTip1Amt(5);
+		setTip2Amt(5);
+	};
+
 	return (
 		<>
 			<div>the Tip Calculator:</div>
-			<InputBill />
+			<InputBill billAmt={billAmt} setBillAmt={setBillAmt} />
 
-			<TipService>
+			<TipService tipAmt={tip1Amt} setTipAmt={setTip1Amt}>
 				<p>How did you like the service?</p>
 			</TipService>
 
-			<TipService>
+			<TipService tipAmt={tip2Amt} setTipAmt={setTip2Amt}>
 				<p>How did your friend like the service?</p>
 			</TipService>
-			<CalcDisplay />
-			<ResetButton />
+			<CalcDisplay billAmt={billAmt} tip1Amt={tip1Amt} tip2Amt={tip2Amt} />
+			<ResetButton onClick={handleResetPress} />
 		</>
 	);
 }

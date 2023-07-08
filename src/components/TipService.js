4a06ebc7fab4ae09dc7e00a763bcb7a4ca72672a
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-08 16:13:12 -0700

    add Tip Input component -mostly complete

--- src/components/TipService.js
+++ src/components/TipService.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function TipService({ children }) {
-	const [tipAmt, setTipAmt] = useState(5);
-
+function TipService({ children, tipAmt, setTipAmt }) {
 	return (
 		<>
-			<span>{children}</span>
-			<select value={tipAmt} onChange={(e) => setTipAmt(e.target.value)}>
-				<option value={5}>It was poor (5%)</option>
-				<option value={10}>It was good (10%)</option>
-				<option value={15}>It was great (15%)</option>
-				<option value={25}>Absolutely amazing! (25%)</option>
-			</select>
+			<div>
+				<span>{children}</span>
+				<select
+					value={tipAmt}
+					onChange={(e) => setTipAmt(Number(e.target.value))}
+				>
+					<option value={5}>It was poor (5%)</option>
+					<option value={10}>It was good (10%)</option>
+					<option value={15}>It was great (15%)</option>
+					<option value={25}>Absolutely amazing! (25%)</option>
+				</select>
+			</div>
 		</>
 	);
 }
